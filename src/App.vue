6de14dd6ffeb49888286c19e0919235d25f626e1
Dennis William <denniswilliam1419@gmail.com>
2026-03-15 02:09:57 +0700

    modified footer

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,32 @@ import Tabs from './components/Tabs.vue'
   <main class="bg-[#F0FDF4]">
     <Header />
     <Tabs class="px-4" />
-    <div class="flex flex-row gap-4 justify-center text-sm text-gray-400 py-6">
-      {{ $t('github') }}
-      <a href="https://github.com/dennwill" target="_blank" rel="noopener noreferrer">
-        <img src="/github-black.png" alt="Visit Dennwill's GitHub profile" width="24" height="24" />
-      </a>
+    <div
+      class="flex flex-col gap-1 justify-center items-center text-sm text-center text-gray-400 py-6"
+    >
+      <div class="flex flex-row">
+        {{ $t('github') }}
+        <a href="https://github.com/dennwill" target="_blank" rel="noopener noreferrer">
+          <img
+            src="/github-black.png"
+            alt="Visit Dennwill's GitHub profile"
+            width="24"
+            height="24"
+            style="margin-left: 10px"
+          />
+        </a>
+      </div>
+      <div class="flex flex-row">
+        {{ $t('donation') }}
+        <a
+          href="https://saweria.co/dennwill"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="font-bold ml-1 hover:underline"
+        >
+          {{ $t('support') }}
+        </a>
+      </div>
     </div>
   </main>
 </template>
